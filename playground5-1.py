f210d385eed391dbd69792004891fca440afe3a6
pomfy113 <pomfy113@gmail.com>
2017-12-28 03:02:18 -0800

    That's enough for tonight

--- playground5-1.py
+++ playground5-1.py
@@ -1,20 +1,103 @@
-# coord = ((20, 30))
-#
-# for x, y in coord:
-#     print(x, y)
-
 from collections import deque
 
-x = deque()
-x.append(1)
-x.append(2)
-x.append(3)
-print(x)
-x.popleft()
-print(x)
-x.popleft()
+def memodict(f):
+     """ Memoization decorator for a function taking a single argument """
+     class memodict(dict):
 
-if x:
-    print("True")
-else:
-    print("False")
+        def __missing__(self, key):
+           ret = self[key] = f(key)
+           return ret
+     return memodict().__getitem__
+
+
+@memodict
+def adjacent_to((maze_dim, point)):
+     neighbors = (
+         (point[0] - 1, point[1]),
+         (point[0], point[1] - 1),
+         (point[0], point[1] + 1),
+         (point[0] + 1, point[1]))
+
+     return [p for p in neighbors if 0 <= p[0] < maze_dim[0] and 0 <= p[1] < maze_dim[1]]
+
+
+
+
+def removable(maz, ii, jj):
+     counter = 0
+     for p in adjacent_to(((len(maz), len(maz[0])), (ii, jj))):
+         if not maz[p[0]][p[1]]:
+             if counter:
+                 return True
+             counter += 1
+     return False
+
+
+def answer(maze):
+
+     path_length = 0
+
+     if not maze:
+         return
+
+     dims = (len(maze), len(maze[0]))
+     end_point = (dims[0]-1, dims[1]-1)
+
+     # list of walls that can be removed
+     passable_walls = set()
+     for i in xrange(dims[0]):
+         for j in xrange(dims[1]):
+             if maze[i][j] == 1 and removable(maze, i, j):
+                 passable_walls.add((i, j))
+
+     shortest_path = 0
+     print(passable_walls)
+     best_possible = dims[0] + dims[1] - 1
+
+     path_mat = [[None] * dims[1] for _ in xrange(dims[0])]  # tracker      matrix for shortest path
+     path_mat[dims[0]-1][dims[1]-1] = 0  # set the starting point to destination (lower right corner)
+
+     for wall in passable_walls:
+         temp_maze = maze
+         if wall:
+             temp_maze[wall[0]][wall[1]] = 0
+
+         stat_mat = [['-'] * dims[1] for _ in xrange(dims[0])]  # status of visited and non visited cells
+
+         q = deque()
+         q.append(end_point)
+
+         while q:
+             curr = q.popleft()
+
+             if curr == (0,0):
+                 break
+
+             for next in adjacent_to((dims, curr)):
+                 if temp_maze[next[0]][next[1]] == 0:  # Not a wall
+                     temp = path_mat[curr[0]][curr[1]] + 1
+                     if temp < path_mat[next[0]][next[1]] or path_mat[next[0]][next[1]] == None:  # there is a shorter path to this cell
+                         path_mat[next[0]][next[1]] = temp
+                     if stat_mat[next[0]][next[1]] != '+':  # Not visited yet
+                         q.append(next)
+
+             stat_mat[curr[0]][curr[1]] = '+'  # mark it as visited
+
+         if path_mat[0][0]+1 <= best_possible:
+             break
+     for i in range(len(path_mat[0])):
+         print(path_mat[i])
+     if shortest_path == 0 or path_mat[0][0]+1 < shortest_path:
+         shortest_path = path_mat[0][0]+1
+
+     return shortest_path
+
+maze = [
+    [0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0]
+    ]
+print(answer(maze))
--- playground5.py
+++ playground5.py
@@ -1,9 +1,22 @@
 from collections import deque
 
+def memoize(f):
+    """Memoize! God, it's so fast."""
+    class memodict(dict):
+        def __getitem__(self, *key):
+            return dict.__getitem__(self, key)
+        def __missing__(self, key):
+            ret = self[key] = f(*key)
+            return ret
+    return memodict().__getitem__
+
 def answer(maze):
+    if not maze:
+        return
     dimensions = (len(maze), len(maze[0]))
     # Set of walls we can pass
     passable = wallcheck(dimensions, maze)
+    print(passable)
     # Actual pathfinding
     return pathfinding(dimensions, maze, passable)
 
@@ -17,7 +30,6 @@ def wallcheck(dim, maze):
     for x in xrange(dim[0]):
         for y in xrange(dim[1]):
             if maze[x][y] == 1 and isremovable(dim, maze, x, y):
-                # Success! We found a passable block
                 passable.add((x, y))
     return passable
 
@@ -32,15 +44,14 @@ def isremovable(dim, maze, x, y):
     # Checks to see all VALID adjacent blocks
     for coord in adjacent_blocks(dim, coordinates):
         if maze[coord[0]][coord[1]] == 0:
+            if adjacent:
+                return True
             adjacent += 1
-
     # We need 2 or more free blocks, otherwise it's pointless
-    if adjacent > 1:
-        return True
-    else:
-        return False
+    return False
 
 
+@memoize
 def adjacent_blocks(dim, point):
     """Set up valid adjacent blocks."""
     adjacent_blocks = (
@@ -51,60 +62,92 @@ def adjacent_blocks(dim, point):
         )
 
     # Check if it's within bounds of maze dimensions
-    for block in adjacent_blocks:
-        if 0 <= block[0] < dim[0] and 0 <= block[1] < dim[1]:
-            yield block
+    return [block for block in adjacent_blocks if 0 <= block[0] < dim[0] and 0 <= block[1] < dim[1]]
+
+
 
 # # # # # # # # #
 #  Pathfinding  #
 # # # # # # # # #
-def pathfinding(dim, maze, passable):
-    end = (dim[0]-1, dim[1]-1)
+def pathfinding(dims, maze, passable):
+    """Actual pathfinding function."""
+    end = (dims[0]-1, dims[1]-1)
     best = 0
 
-    mazemap = [[None] * dim[1] for _ in xrange(dim[0])]
-    mazemap[end[0]][end[1]] = 0          # Starting and ending
+    mazemap = [[None] * dims[1] for _ in xrange(dims[0])]
+    # Starting at the end, then working our way back!
+    mazemap[dims[0]-1][dims[1]-1] = 0
 
+    # Check the passable walls
     for wall in passable:
         temp_maze = maze
+        # If we can break it, let's!
+        # We checked to make sure ALL passable walls are valid
         if wall:
             temp_maze[wall[0]][wall[1]] = 0
 
-        stat_mat = [['-'] * dim[1] for _ in xrange(dim[0])]
-        #
+
+        # For checking visited/non-visited
+        stat_mat = [['-'] * dims[1] for _ in xrange(dims[0])]
+
+        # Queue for where to go next
+        # Originally list, but deques have O(n) queue/dequeue
         queue = deque()
         queue.append(end)
-    while queue:
-        current = queue.popleft()
+        moves = 0
+        while queue:
+            # Dequeue; where we go next
+            current = queue.popleft()
 
-        if current == (0, 0):
+            # We hit the beginning point
+            if current == (0, 0):
+                break
+
+            # Check where the next block would take us
+            for nextblock in adjacent_blocks(dims, current):
+                next_x = nextblock[0]
+                next_y = nextblock[1]
+                # Check if not a wall
+                if temp_maze[next_x][next_y] == 0:
+                    # If so, that's our next move! Add 1 to move
+                    moves = mazemap[current[0]][current[1]] + 1
+                    if moves < mazemap[next_x][next_y] or mazemap[next_x][next_y] is None:  # there is a shorter path to this cell
+                        mazemap[next_x][next_y] = moves
+                    if stat_mat[next_x][next_y] != 'x':  # Not visited yet
+                        queue.append(nextblock)
+
+            stat_mat[current[0]][current[1]] = 'x'  # mark it as visited
+
+        if mazemap[0][0]+1 <= (dims[0] + dims[1] - 1):
             break
+    for i in range(len(mazemap[0])):
+        print(mazemap[i])
 
-        for next in adjacent_blocks(dim, current):
-            if temp_maze[next[0]][next[1]] == 0:  # Not a wall
-                temp = mazemap[current[0]][current[1]] + 1
-                if temp < mazemap[next[0]][next[1]] or mazemap[next[0]][next[1]] == None:  # there is a shorter path to this cell
-                    mazemap[next[0]][next[1]] = temp
-                if stat_mat[next[0]][next[1]] != '+':  # Not visited yet
-                    queue.append(next)
-        print(queue)
-        mazemap[current[0]][current[1]] = '+'
-
-        # That's as fast as we can go
-        if mazemap[0][0]+1 <= (dim[0] + dim[1] - 1):
-            break
     if best == 0 or mazemap[0][0]+1 < best:
         best = mazemap[0][0]+1
 
-    return best
+        return best
 
 maze = [
-    [0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 1],
-    [0, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0]
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ]
 
 print(answer(maze))
