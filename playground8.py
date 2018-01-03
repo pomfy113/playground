7b43f19ee8fbb82c55b67e782f7fc42324b15e2f
pomfy113 <pomfy113@gmail.com>
2018-01-02 21:55:42 -0800

    Basic hitbox started

--- playground8.py
+++ playground8.py
@@ -13,14 +13,40 @@ WALL_L = line((-1, -1), (-1, 0))
 WALL_R = None
 FLOOR = None
 
+def hitbox_set(your_x, your_y, guard_x, guard_y):
+        global GUARD_L, GUARD_R, GUARD_U, GUARD_D
+
+        GUARD_L = line((guard_x-0.5, guard_y-0.5), (guard_x-0.5, guard_y+0.5))
+        GUARD_R = line((guard_x+0.5, guard_y-0.5), (guard_x+0.5, guard_y+0.5))
+        GUARD_U = line((guard_x-0.5, guard_y-0.5), (guard_x-0.5, guard_y+0.5))
+        GUARD_D = line((guard_x-0.5, guard_y+0.5), (guard_x+0.5, guard_y+0.5))
+
+        global YOUR_L, YOUR_R, YOUR_U, YOUR_D
+
+        YOUR_L = line((your_x-0.5, your_y-0.5), (your_x-0.5, your_y+0.5))
+        YOUR_R = line((your_x+0.5, your_y-0.5), (your_x+0.5, your_y+0.5))
+        YOUR_U = line((your_x-0.5, your_y-0.5), (your_x-0.5, your_y+0.5))
+        YOUR_D = ((your_x-0.5, your_y+0.5), (your_x+0.5, your_y+0.5))
+
+
 def answer(dimensions, your_position, guard_position, distance):
     """Begin combat protocol."""
     global WALL_R, FLOOR
+    global YOU, GUARD
+
     WALL_R = line((dimensions[0]+1, -1), (dimensions[0]+1, 0))
     FLOOR = line((0, dimensions[1]+1), (-1, dimensions[1]+1))
 
+    YOU = your_position
+    GUARD = guard_position
+
     your_x = your_position[0]
     your_y = your_position[1]
+    guard_x = guard_position[0]
+    guard_y = guard_position[1]
+
+    hitbox_set(your_x, your_y, guard_x, guard_y)
+
     distance_between = get_distance(your_position, guard_position)
 
     # Easiest cases; enough distance or not enough distance
@@ -36,31 +62,38 @@ def answer(dimensions, your_position, guard_position, distance):
     laser_x = your_x
     laser_y = your_y - distance
     answers.append((laser_x, laser_y))
-
+    answers.append((3, -2))
     while laser_y != your_y:
         laser_x += 1
         laser_y = your_y - missing_angle(laser_x, distance)  # Offset
         answers.append((laser_x, laser_y))
     for item in answers:
         print("My position:", your_position)
-        print("Laser direction:", (laser_x, laser_y))
+        print("Laser direction:", item)
         print("Target position:", guard_position)
         reflect(dimensions, your_position, guard_position, item, distance)
-        print(is_on(your_position, (laser_x, laser_y), guard_position))
+        print(is_on(your_position, item, guard_position))
         print("======================")
     return answers
 
+def hit_check(laser):
+    if intersection
 
-def reflect(dim, origin, target, laser, dist):
+def reflect(dim, origin, laser, dist):
     # Going toward ceiling
     laser_line = line(origin, laser)
-    if is_on(origin, laser, target):
-        print("YOU HIT THE TARGET!")
+    if is_on(origin, laser):
+        print("* * * * * YOU HIT THE TARGET! * * * * *")
+    if dist <= 0:
+        print("Out of distance")
+        return
+
 
     # See if floor or ceiling
     if origin[1] == laser[1]:
         vert_line = None
     elif origin[1] > laser[1]:
+        print(origin[1], "SHOULD BE MORE THAN", laser[1])
         vert_line = CEILING
         vert_hit = intersection(laser_line, vert_line)
         vert_dist = get_distance(origin, vert_hit)
@@ -68,7 +101,6 @@ def reflect(dim, origin, target, laser, dist):
         vert_line = FLOOR
         vert_hit = intersection(laser_line, vert_line)
         vert_dist = get_distance(origin, vert_hit)
-
 
     # See if it goes left or right
     if laser[0] == origin[0]:
@@ -100,9 +132,16 @@ def reflect(dim, origin, target, laser, dist):
         # Wall is further
         if vert_dist < wall_dist:
             if vert_line == FLOOR:
-                print("... but you're hitting the floor first")
+                print("... but you're hitting the ceiling first at", vert_hit)
+                print("Angle", origin, vert_hit)
+                print("Distance left, laser direction", dist - vert_dist, laser)
+                reflect(dim, vert_hit, (laser[0] + vert_hit[0], -(laser[1] + vert_hit[1])), dist - vert_dist)
+
             elif vert_line == CEILING:
-                print("... but you're hitting the ceiling first")
+                print("... but you're hitting the ceiling first at", vert_hit)
+                print("Angle", origin, vert_hit)
+                print("Distance left, laser direction", dist - vert_dist, laser)
+                reflect(dim, vert_hit, (laser[0] + vert_hit[0], -(laser[1] + vert_hit[1])), dist - vert_dist)
         # Floor/Ceiling is further
         elif vert_dist > wall_dist:
             if wall_line == WALL_L:
@@ -112,10 +151,10 @@ def reflect(dim, origin, target, laser, dist):
 
 
 # INTERSECTIONS: I'll need to learn about this later
-def intersection(laser_line, vert_line):
-    D  = laser_line[0] * vert_line[1] - laser_line[1] * vert_line[0]
-    Dx = laser_line[2] * vert_line[1] - laser_line[1] * vert_line[2]
-    Dy = laser_line[0] * vert_line[2] - laser_line[2] * vert_line[0]
+def intersection(laser_line, target_line):
+    D  = laser_line[0] * target_line[1] - laser_line[1] * target_line[0]
+    Dx = laser_line[2] * target_line[1] - laser_line[1] * target_line[2]
+    Dy = laser_line[0] * target_line[2] - laser_line[2] * target_line[0]
     if D != 0:
         x = Dx / D
         y = Dy / D
@@ -134,6 +173,7 @@ def get_distance(pt1, pt2):
     y_dist = pt1[1] - pt2[1]
     return math.sqrt((x_dist**2) + (y_dist**2))
 
+# Checking to see if something is on a line
 def is_on(a, b, c):
     """See if it's between a and b. x=[0], y=[1]"""
     # Use either the x or the y to see if within bounds
